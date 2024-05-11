ddea853e93df4c8968ec7efd14c4f0cdcaa33ffb
Adari Dinesh Srisanth <adsrisanth2005@gmail.com>
2024-05-12 02:34:01 +0530

    Updated_home

--- frontend/pages/home.vue
+++ frontend/pages/home.vue
@@ -12,7 +12,7 @@
         <NavBar /> 
         <div class="flex justify-center items-center w-full min-h-screen bg-slate-200 dark:bg-gray-900 dark:text-white text-black" v-if="!isLoading">
             <div class="flex flex-col gap gap-5 text-center"> 
-                <span class="text-[50px] font-mono font-bold">WebApps Club</span>
+                <span class="text-[62px] font-mono font-bold">WebAppsClub</span>
                 <span class="text-[24px] font-montserrat">- Decentralized -</span>
             </div>
         </div>
